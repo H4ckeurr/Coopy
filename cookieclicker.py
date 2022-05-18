613622abfed7f2ff6c88ef035c01c8c3d2f37d34
H4ckeur <74295077+H4ckeurr@users.noreply.github.com>
2022-05-18 07:40:27 +0200

    added cheat codes
    just for fun, will really clean the code next time tho

--- cookieclicker.py
+++ cookieclicker.py
@@ -8,7 +8,7 @@ from time import time, sleep
 #Main window declaration and configuration
 gameWindow = tk.Tk()
 gameWindow.title("Coopy Clicker")
-gameWindow.geometry("500x450")
+gameWindow.geometry("500x430")
 gameWindow.resizable(False, False)
 
 
@@ -21,6 +21,32 @@ mainFrame.grid(column=1, row=0, sticky=tk.N, padx=5)
 itemShop = tk.Frame(gameWindow, borderwidth=2, relief=tk.GROOVE)
 itemShop.grid(column=1, row=0, sticky=tk.W, padx=5)
 itemShop.grid_remove()
+
+infosFrame = tk.Frame(gameWindow, borderwidth=2, relief=tk.GROOVE)
+infosFrame.grid(column=1, row=0, sticky=tk.S, padx=5, pady=30)
+
+settingsFrame = tk.Frame(gameWindow, borderwidth=2, relief=tk.GROOVE)
+settingsFrame.grid(column=1, row=0, sticky=tk.S, padx=5, pady=30)
+settingsFrame.grid_remove()
+
+cheatLabel = tk.Label(settingsFrame, text=f"Activer code de triche")
+cheatLabel.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+
+cheatCode = tk.StringVar()
+cheatEntry = tk.Entry(settingsFrame, textvariable=cheatCode)
+cheatEntry.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
+
+def activateCheatCode():
+    global cheatCode, gold
+    if(cheatCode.get() == "iamrich"):
+        gold = 1000000
+        messagebox.showinfo("Succès", "Code de triche activé")
+    else:
+        messagebox.showerror("Erreur", "Code inconnu")
+
+cheatButton = tk.Button(settingsFrame, text=f"Activer", command=activateCheatCode)
+cheatButton.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
+
 
 gold = 0
 
@@ -36,7 +62,7 @@ moreAutoClicker = None
 goldUpgradeNumber = 5
 
 #Declaring upgrade prices variables
-goldsForGoldUpgrade = 20
+goldsForGoldUpgrade = 10
 goldsForAutoClicker = 50
 
 autoClickerAmount = 0
@@ -75,16 +101,16 @@ def contract():
 def fill():
     if expanded:
         # Show a text and remove the image
-        homeButton.config(text="Accueil", image="")
-        shopButton.config(text="Améliorations", image="")
-        settingsButton.config(text="Paramètres", image="")
+        homeButton.config(text="Accueil", image="", width = 14)
+        shopButton.config(text="Améliorations", image="", width = 14)
+        settingsButton.config(text="Paramètres", image="", width = 14)
         username.config(text=f"Coopy Clicker\n\nDéveloppé par\nBrayan\nAlex\nYassine")
         
     else:
         # Bring the image back
-        homeButton.config(image=homeImage)
-        shopButton.config(image=shopImage)
-        settingsButton.config(image=settingsImage)
+        homeButton.config(image=homeImage, width = 25)
+        shopButton.config(image=shopImage, width = 25)
+        settingsButton.config(image=settingsImage, width = 25)
         username.config(text=f"")
         
 # Define the icons to be shown
@@ -126,14 +152,18 @@ def leave(item):
 Using grid_remove() instead of grid_forget() allows us to keep all the settings, so using grid() will show it back without the need of specifying all the options again
 """
 def showMainFrame():
-    global mainFrame, itemShop
+    global mainFrame, itemShop, infosFrame, settingsFrame
     mainFrame.grid()
+    infosFrame.grid()
     itemShop.grid_remove()
+    settingsFrame.grid_remove()
 
 def showShopFrame():
-    global mainFrame, moreGold, itemShop, goldsForGoldUpgrade, moreAutoClicker, goldsForAutoClicker, moreGoldInfoLabel, autoClickerAmount, moreAutoclickerInfoLabel, gold
+    global mainFrame, infosFrame, settingsFrame, moreGold, itemShop, goldsForGoldUpgrade, moreAutoClicker, goldsForAutoClicker, moreGoldInfoLabel, autoClickerAmount, moreAutoclickerInfoLabel, gold
 
     mainFrame.grid_remove()
+    infosFrame.grid_remove()
+    settingsFrame.grid_remove()
     itemShop.grid()
 
     moreGoldUpgrade = tk.Frame(itemShop, borderwidth=2, relief=tk.GROOVE)
@@ -155,6 +185,14 @@ def showShopFrame():
     moreAutoClicker.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
     moreAutoClicker.bind("<ButtonRelease-1>", upgradeAutoClicker)
     moreAutoClicker.bind("<Leave>", leave)
+    
+
+def showSettingsFrame():
+    global mainFrame, itemShop, infosFrame, settingsFrame
+    settingsFrame.grid()
+    mainFrame.grid_remove()
+    infosFrame.grid_remove()
+    itemShop.grid_remove()
 
 #This is used to add gold, you have 50% chance to get between 1 and the upgrade number(default is 5), if you're unlucky you get 0
 def incr(event):
@@ -171,7 +209,8 @@ def upgradeAutoClicker(event):
     global goldUpgradeNumber, moreGold, gold, goldsForAutoClicker, autoClickerAmount, moreAutoclickerInfoLabel
     if(gold >= goldsForAutoClicker):
         gold-=goldsForAutoClicker
-        goldsForAutoClicker*=3
+        goldsForAutoClicker*=1.15
+        goldsForAutoClicker=int(goldsForAutoClicker)
         autoClickerAmount+=1
     moreAutoclickerInfoLabel.config(text=f"Un ouvrier qui génère 1 d'or/s\nActuel : {autoClickerAmount} or/s\nCoûte : {goldsForAutoClicker} or")
     autoclickerLabel.config(text=f"Ouvriers : {autoClickerAmount}")
@@ -193,7 +232,8 @@ def upgradeGold(event):
     if(gold >= goldsForGoldUpgrade):
         gold-=goldsForGoldUpgrade
         goldUpgradeNumber*=2
-        goldsForGoldUpgrade*=3
+        goldsForGoldUpgrade*=2.5
+        goldsForGoldUpgrade=int(goldsForGoldUpgrade)
     moreGoldInfoLabel.config(text=f"Augemente l'or par clic\nActuel : {goldUpgradeNumber} or par clic\nCoûte : {goldsForGoldUpgrade} or")
     counterLabel.config(text=f"Cookies mangés : {totalCookies}")
     goldLabel.config(text=f"Or : {gold}")
@@ -212,6 +252,7 @@ username = tk.Label(sideMenu, text=f"", bg="orange")
 username.grid(column=0, row=3, padx=10, pady=200)
 
 photo = tk.PhotoImage(file="cookie.png")
+
 incrButton = tk.Button(mainFrame, image=photo, relief=tk.FLAT, compound=tk.TOP, width=256, height=266)
 incrButton.grid(column=1, row=1, sticky=tk.W, padx=15, pady=5)
 incrButton.bind("<ButtonRelease-1>", incr)
@@ -223,12 +264,9 @@ homeButton.grid(column=0, row=0, pady=10)
 shopButton = tk.Button(sideMenu, image=shopImage, bg="orange", relief=tk.FLAT, command=openShop)
 shopButton.grid(column=0, row=1)
 
-settingsButton = tk.Button(sideMenu, image=settingsImage, bg="orange", relief=tk.FLAT)
+settingsButton = tk.Button(sideMenu, image=settingsImage, bg="orange", relief=tk.FLAT, command=showSettingsFrame)
 settingsButton.grid(column=0, row=2, pady=10)
 
-
-infosFrame = tk.Frame(mainFrame, borderwidth=2, relief=tk.GROOVE)
-infosFrame.grid(column=1, row=0, sticky=tk.W, padx=5)
 
 counterLabel = tk.Label(infosFrame, text="Cookies mangés : 0")
 counterLabel.grid(column=1, row=2, sticky=tk.W, padx=5, pady=5)
